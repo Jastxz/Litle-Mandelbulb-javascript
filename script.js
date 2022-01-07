73d3806222fe01a079b2fcdaf78afadddc6436e6
Jastxz <53936809+Jastxz@users.noreply.github.com>
2022-01-07 16:54:42 +0100

    Comentarios y variables en español

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ Este código está inspirado y ayudado por el código de los siguientes enlaces:
   - http://www.redcode.nl
 */
 
+//Constantes y variables globales
 const formula = document.getElementById("formula");
 var numero;
 var archivo = "/potencia.txt";
-var currenty = 0.0;
-var context;
+var actual = 0.0;
+var contexto;
 var image;
 var imageData;
 var cHeight;
@@ -16,39 +17,43 @@ var cWidth;
 var f = new Date().getTime();
 var NUL = [0.0, 0.0, 0.0];
 var z = [0.0, 0.0, 0.0];
-var Iterations = 20.0;
-var Power;
+var Iteraciones = 20.0;
+var Potencia;
 var last = 0;
 var sphereZ = [0, 0, 0];
 var MAX_ITER = 5000.0;
 var DEPTH_OF_FIELD = 3.5;
-var eyeDistanceFromNearField = 2.5; //Para acercar o alejar la vista
-var halfPixel;
+var distanciaVisionCercana = 2.5; //Para acercar o alejar la vista
+var mitadPixel;
 var pixel;
-var lightAngle = 140.0;
-var viewAngle = 150.0;
-var smallStep = 0.01;
-var bigStep = 0.02;
-var lightLocation = [0.0, 0.0, 0.0];
-var lightDirection = [0.0, 0.0, 0.0];
-var nearFieldLocation = [0.0, 0.0, 0.0];
-var viewDirection = [0.0, 0.0, 0.0];
+var anguloLuz = 140.0;
+var anguloEscena = 150.0;
+var pasoPequeño = 0.01;
+var pasoGrande = 0.02;
+var localizacionLuz = [0.0, 0.0, 0.0];
+var direccionLuz = [0.0, 0.0, 0.0];
+var campoMásCercano = [0.0, 0.0, 0.0];
+var direccionVista = [0.0, 0.0, 0.0];
 var reverseDirection = [0.0, 0.0, 0.0];
 var eyeLocation = [0.0, 0.0, 0.0];
 var pixelLocation = [0.0, 0.0, 0.0];
 var rayLocation = [0.0, 0.0, 0.0];
-var tempViewDirectionX1 = [0.0, 0.0, 0.0];
-var tempViewDirectionX2 = [0.0, 0.0, 0.0];
-var tempViewDirectionY = [0.0, 0.0, 0.0];
-var rayDirection = [0.0, 0.0, 0.0];
+var tempDireccionVistaX1 = [0.0, 0.0, 0.0];
+var tempDireccionVistaX2 = [0.0, 0.0, 0.0];
+var tempDireccionVistaY = [0.0, 0.0, 0.0];
+var direccionRayo = [0.0, 0.0, 0.0];
 var normal = [0.0, 0.0, 0.0];
-var halfway = [0.0, 0.0, 0.0];
+var vectorNulo = [0.0, 0.0, 0.0];
 var temp = [0.0, 0.0, 0.0];
 var ro = [0.0, 0.0, 0.0];
 var rd = [0.0, 0.0, 0.0];
 
 window.onload = function () {
-  Power = 8;
+  
+  // Por defecto mostramos el Mandelbulb normal
+  Potencia = 8;
+  
+  //Recogemos los datos de la url si los hubiera
   var query = window.location.search.substring(1);
   var parms = query.split('#');//Si pasas más de una variable, usas un separador, como #
   for (var i=0; i<parms.length; i++)
@@ -57,59 +62,66 @@ window.onload = function () {
     if (pos > 0)
     {
       var val = parms[i].substring(pos+1);
-      Power = parseFloat(val);
+      Potencia = parseFloat(val);
     }
   }
-  console.log(Power)
+
+  //Comienza el juego
   iniciaAnimacion();
 };
-//Función para cambiar la potencia de z y poder observar otros fractales
+
+//Función para cambiar la potencia de z y poder observar otros fractales mediante el campo
+// de texto del HTML
 formula.addEventListener("keyup", ({key}) => {
   if (key === "Enter") {
     numero = parseFloat(formula.value);
-	  location.href=`?Power=${numero}`;
+	  location.href=`?Potencia=${numero}`;
   }
 });
 
+//Preparamos el canvas de la imagen y comenzamos la animación
 function iniciaAnimacion() {
   //Preparamos el lugar de la imagen
   var mandelbulbCanvas = document.getElementById("mandelbulto");
   cHeight = mandelbulbCanvas.height;
   cWidth = mandelbulbCanvas.width;
-  context = mandelbulbCanvas.getContext("2d");
-  context.fillRect(0, 0, cWidth, cHeight);
+  contexto = mandelbulbCanvas.getcontexto("2d");
+  contexto.fillRect(0, 0, cWidth, cHeight);
 
   //Con una profundidad de campo de 2.0x2.0 calculamos el detalle del pixel
   pixel = DEPTH_OF_FIELD / ((cHeight + cWidth) / 2);
-  halfPixel = pixel / 2;
-  image = context.getImageData(0, 0, cWidth, cHeight);
+  mitadPixel = pixel / 2;
+  image = contexto.getImageData(0, 0, cWidth, cHeight);
   imageData = image.data;
 
   anima();
 }
 
+//
 function anima() {
   //Iniciamos el ángulo y la escena
-  if (currenty == 0) {
+  if (actual == 0) {
     animaCamara();
     iniciaEscena();
   }
 
-  // Dibujamos las líneas de nuestra imagen
+  // Dibujamos las líneas de nuestro fractal
   var start = new Date().getTime();
-  while (currenty < cHeight && new Date().getTime() - start < 500) {
-    imageData = draw(imageData, currenty++);
+  while (actual < cHeight && new Date().getTime() - start < 500) {
+    imageData = dibuja(imageData, actual++);
   }
 
-  //Si llegamos al límite devolvemos el número del píxel a 0
-  if (currenty >= cHeight) {
-    currenty = 0;
+  //Si llegamos al límite devolvemos el número del píxel a 0 y el tiempo
+  // que hemos tardado en dibujar toda la escena
+  if (actual >= cHeight) {
+    actual = 0;
     console.log("Took:" + (new Date().getTime() - f));
     f = new Date().getTime();
   }
 
+  //Actualizamos valores
   image.data = imageData;
-  context.putImageData(image, 0, 0);
+  contexto.putImageData(image, 0, 0);
 
   //Le otorgamos recursividad infinita
   requestAnimFrame(function () {
@@ -117,6 +129,7 @@ function anima() {
   });
 }
 
+//Le pedimos al navegador que no se olvide de dibujar
 window.requestAnimFrame = (function (callback) {
   return (
     window.requestAnimationFrame ||
@@ -137,112 +150,107 @@ function map(z) {
 /*La siguiente función proviene de:
         http://blog.hvidtfeldts.net/index.php/2011/09/distance-estimated-3d-fractals-v-the-mandelbulb-different-de-approximations/*/
 function mandelbulb(pos) {
-  //Primero obtenemos los valores de la función requerida
-  //gestionaFormula()
 
-  //Cálculos
+  //Cálculos de forma iterativa para la distancia del fractal
   igualaV1yV2(z, pos);
   var dr = 1.0;
   var r = 0.0;
-  for (var i = 0; i < Iterations; i++) {
-    r = length(z);
+  for (var i = 0; i < Iteraciones; i++) {
+    r = modulo(z);
     if (r > DEPTH_OF_FIELD) break;
 
-    // convert to polar coordinates
+    // Convertimos dr a coordenadas polares
     var theta = Math.acos(z[2] / r);
     var phi = Math.atan2(z[1], z[0]);
-    dr = Math.pow(r, Power - 1.0) * Power * dr + 1.0;
+    dr = Math.pow(r, Potencia - 1.0) * Potencia * dr + 1.0;
 
-    // scale and rotate the point
-    var zr = Math.pow(r, Power);
-    theta = theta * Power;
-    phi = phi * Power;
+    // Escalamos y rotamos los puntos
+    var zr = Math.pow(r, Potencia);
+    theta = theta * Potencia;
+    phi = phi * Potencia;
 
-    // convert back to cartesian coordinates
+    // Volvemos a las coordenadas cartesianas
     var sinTheta = Math.sin(theta);
     z[0] = sinTheta * Math.cos(phi);
     z[1] = Math.sin(phi) * sinTheta;
     z[2] = Math.cos(theta);
-    sumaVectores(scalarMultiply(z, zr), pos);
+    sumaVectores(multiplicacionEscalar(z, zr), pos);
   }
   return (0.5 * Math.log(r) * r) / dr;
 }
-//This method takes viewAngle and lightAngle and repositions
+
+//Con este método reposicionamos la luz y la escena en el tiempo
 function iniciaEscena() {
-  var rad = toRad(lightAngle);
+  var rad = aRad(anguloLuz);
   var lightX = (Math.cos(rad) * DEPTH_OF_FIELD) / 2;
   var lightZ = (Math.sin(rad) * DEPTH_OF_FIELD) / 2;
 
-  lightLocation[0] = lightX;
-  lightLocation[1] = DEPTH_OF_FIELD / 2;
-  lightLocation[2] = lightZ;
+  localizacionLuz[0] = lightX;
+  localizacionLuz[1] = DEPTH_OF_FIELD / 2;
+  localizacionLuz[2] = lightZ;
 
-  normalize(subtract(igualaV1yV2(lightDirection, NUL), lightLocation));
+  normaliza(restaVectores(igualaV1yV2(direccionLuz, NUL), localizacionLuz));
 
-  var viewRad = toRad(viewAngle);
+  var viewRad = aRad(anguloEscena);
   var viewX = (Math.cos(viewRad) * DEPTH_OF_FIELD) / 2;
   var viewZ = (Math.sin(viewRad) * DEPTH_OF_FIELD) / 2;
 
-  nearFieldLocation[0] = viewX;
-  nearFieldLocation[1] = 0.0;
-  nearFieldLocation[2] = viewZ;
+  campoMásCercano[0] = viewX;
+  campoMásCercano[1] = 0.0;
+  campoMásCercano[2] = viewZ;
 
-  normalize(subtract(igualaV1yV2(viewDirection, NUL), nearFieldLocation));
+  normaliza(restaVectores(igualaV1yV2(direccionVista, NUL), campoMásCercano));
 
-  scalarMultiply(
-    igualaV1yV2(reverseDirection, viewDirection),
-    eyeDistanceFromNearField
+  multiplicacionEscalar(
+    igualaV1yV2(reverseDirection, direccionVista),
+    distanciaVisionCercana
   );
-  subtract(igualaV1yV2(eyeLocation, nearFieldLocation), reverseDirection);
+  restaVectores(igualaV1yV2(eyeLocation, campoMásCercano), reverseDirection);
 }
-//The main draw function for a scanline
-//Make sure setupScene is called first after adjusting the camera and/or light
-function draw(imageData, y) {
+//La función de dibujado principal
+function dibuja(imageData, y) {
   var cHalfWidth = cWidth / 2;
   var ny = y - cHeight / 2;
 
-  scalarMultiply(
-    crossProduct(
-      turnOrthogonal(igualaV1yV2(tempViewDirectionY, viewDirection)),
-      viewDirection
+  multiplicacionEscalar(
+    productoVectorial(
+      vuelveOrtogonal(igualaV1yV2(tempDireccionVistaY, direccionVista)),
+      direccionVista
     ),
     ny * pixel
   );
-  turnOrthogonal(igualaV1yV2(tempViewDirectionX1, viewDirection));
+  vuelveOrtogonal(igualaV1yV2(tempDireccionVistaX1, direccionVista));
 
   for (var x = 0; x < cWidth; x++) {
     var nx = x - cHalfWidth;
 
-    igualaV1yV2(pixelLocation, nearFieldLocation);
+    igualaV1yV2(pixelLocation, campoMásCercano);
 
-    scalarMultiply(
-      igualaV1yV2(tempViewDirectionX2, tempViewDirectionX1),
+    multiplicacionEscalar(
+      igualaV1yV2(tempDireccionVistaX2, tempDireccionVistaX1),
       nx * pixel
     );
-    sumaVectores(pixelLocation, tempViewDirectionX2);
-    sumaVectores(pixelLocation, tempViewDirectionY);
+    sumaVectores(pixelLocation, tempDireccionVistaX2);
+    sumaVectores(pixelLocation, tempDireccionVistaY);
 
     igualaV1yV2(rayLocation, pixelLocation);
 
-    normalize(subtract(igualaV1yV2(rayDirection, rayLocation), eyeLocation));
+    normaliza(restaVectores(igualaV1yV2(direccionRayo, rayLocation), eyeLocation));
 
     var distanceFromCamera = 0.0;
     var d = map(rayLocation);
 
-    var iterations = 0;
-    for (; iterations < MAX_ITER; iterations++) {
-      if (d < halfPixel) {
+    var Iteraciones = 0;
+    for (; Iteraciones < MAX_ITER; Iteraciones++) {
+      if (d < mitadPixel) {
         break;
       }
 
-      //Increase rayLocation with direction and d:
-      sumaVectores(rayLocation, scalarMultiply(rayDirection, d));
-      //And reset ray direction:
-      normalize(rayDirection);
+      sumaVectores(rayLocation, multiplicacionEscalar(direccionRayo, d));
+      normaliza(direccionRayo);
 
-      //Move the pixel location:
-      distanceFromCamera = length(
-        subtract(igualaV1yV2(temp, nearFieldLocation), rayLocation)
+      distanceFromCamera = modulo(
+        restaVectores(igualaV1yV2(temp, campoMásCercano), rayLocation)
       );
 
       if (distanceFromCamera > DEPTH_OF_FIELD) {
@@ -252,41 +260,41 @@ function draw(imageData, y) {
     }
 
     if (distanceFromCamera < DEPTH_OF_FIELD && distanceFromCamera > 0) {
-      rayLocation[0] -= smallStep;
+      rayLocation[0] -= pasoPequeño;
       var locationMinX = map(rayLocation);
-      rayLocation[0] += bigStep;
+      rayLocation[0] += pasoGrande;
       var locationPlusX = map(rayLocation);
-      rayLocation[0] -= smallStep;
+      rayLocation[0] -= pasoPequeño;
 
-      rayLocation[1] -= smallStep;
+      rayLocation[1] -= pasoPequeño;
       var locationMinY = map(rayLocation);
-      rayLocation[1] += bigStep;
+      rayLocation[1] += pasoGrande;
       var locationPlusY = map(rayLocation);
-      rayLocation[1] -= smallStep;
+      rayLocation[1] -= pasoPequeño;
 
-      rayLocation[2] -= smallStep;
+      rayLocation[2] -= pasoPequeño;
       var locationMinZ = map(rayLocation);
-      rayLocation[2] += bigStep;
+      rayLocation[2] += pasoGrande;
       var locationPlusZ = map(rayLocation);
-      rayLocation[2] -= smallStep;
+      rayLocation[2] -= pasoPequeño;
 
-      //Calculate the normal:
+      //Calcula la norma:
       normal[0] = locationMinX - locationPlusX;
       normal[1] = locationMinY - locationPlusY;
       normal[2] = locationMinZ - locationPlusZ;
-      normalize(normal);
+      normaliza(normal);
 
-      //Calculate the ambient light:
-      var dotNL = dotProduct(lightDirection, normal);
-      var diff = saturate(dotNL);
+      //Calcula la luz ambiente:
+      var dotNL = productoEscalar(direccionLuz, normal);
+      var diff = satura(dotNL);
 
-      //Calculate specular light:
-      normalize(
-        sumaVectores(igualaV1yV2(halfway, rayDirection), lightDirection)
+      //Calcula la luz especular:
+      normaliza(
+        sumaVectores(igualaV1yV2(vectorNulo, direccionRayo), direccionLuz)
       );
 
-      var dotNH = dotProduct(halfway, normal);
-      var spec = Math.pow(saturate(dotNH), 35);
+      var dotNH = productoEscalar(vectorNulo, normal);
+      var spec = Math.pow(satura(dotNH), 35);
 
       var shad = shadow(1.0, DEPTH_OF_FIELD, 16.0) + 0.25;
       var brightness = (10.0 + (200.0 + spec * 45.0) * shad * diff) / 270.0;
@@ -295,9 +303,9 @@ function draw(imageData, y) {
       var green = 10 + 280 * brightness;
       var blue = 180 * brightness;
 
-      red = clamp(red, 0, 255.0);
-      green = clamp(green, 0, 255.0);
-      blue = clamp(blue, 0, 255.0);
+      red = restriccion(red, 0, 255.0);
+      green = restriccion(green, 0, 255.0);
+      blue = restriccion(blue, 0, 255.0);
 
       var pixels = 4 * (y * cWidth + x);
       imageData[pixels + 0] = red;
@@ -305,20 +313,21 @@ function draw(imageData, y) {
       imageData[pixels + 2] = blue;
     } else {
       var pixels = 4 * (y * cWidth + x);
-      imageData[pixels + 0] = 155 + clamp(iterations * 1.5, 0.0, 100.0);
-      imageData[pixels + 1] = 205 + clamp(iterations * 1.5, 0.0, 50.0);
+      imageData[pixels + 0] = 155 + restriccion(Iteraciones * 1.5, 0.0, 100.0);
+      imageData[pixels + 1] = 205 + restriccion(Iteraciones * 1.5, 0.0, 50.0);
       imageData[pixels + 2] = 255;
     }
   }
   return imageData;
 }
-//In this method we calculate the 'soft' shadows
-//From: http://www.iquilezles.org/www/articles/rmshadows/rmshadows.htm
+
+//Calculamos las sombras
+//Proveniente de: http://www.iquilezles.org/www/articles/rmshadows/rmshadows.htm
 function shadow(mint, maxt, k) {
   var res = 1.0;
   for (var t = mint; t < maxt; ) {
-    scalarMultiply(igualaV1yV2(rd, lightDirection), t);
-    subtract(igualaV1yV2(ro, rayLocation), rd);
+    multiplicacionEscalar(igualaV1yV2(rd, direccionLuz), t);
+    restaVectores(igualaV1yV2(ro, rayLocation), rd);
     var h = map(ro);
     if (h < 0.001) {
       return 0.0;
@@ -328,44 +337,47 @@ function shadow(mint, maxt, k) {
   }
   return res;
 }
-//Here we change the camera position and light(s)
+
+//Cambiamos la posición de la cámara y las luces
 function animaCamara() {
-  lightAngle += 2.0;
-  lightAngle %= 360.0;
-  viewAngle += 2.0;
-  viewAngle %= 360.0;
+  anguloLuz += 2.0;
+  anguloLuz %= 360.0;
+  anguloEscena += 2.0;
+  anguloEscena %= 360.0;
 }
+
 //Funciones para acercar y alejar la cámara
 function acercaCamara() {
-  eyeDistanceFromNearField += 0.5;
+  distanciaVisionCercana += 0.5;
 }
 function alejaCamara() {
-  if (eyeDistanceFromNearField > 0.5) {
-    eyeDistanceFromNearField -= 0.5;
+  if (distanciaVisionCercana > 0.5) {
+    distanciaVisionCercana -= 0.5;
   } else {
     window.alert("¡Ya no se puede alejar la cámara más!");
   }
 }
+
 //Funciones auxiliares
-function dotProduct(v1, v2) {
+function productoEscalar(v1, v2) {
   return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];
 }
-function toRad(r) {
+function aRad(r) {
   return (r * Math.PI) / 180.0;
 }
-function saturate(n) {
-  return clampVec(n, 0.0, 1.0);
+function satura(n) {
+  return restringeVec(n, 0.0, 1.0);
 }
-function clamp(n, min, max) {
+function restriccion(n, min, max) {
   return Math.max(min, Math.min(n, max));
 }
-function length(z) {
+function modulo(z) {
   return Math.sqrt(z[0] * z[0] + z[1] * z[1] + z[2] * z[2]);
 }
-function normalize(a) {
-  return scalarMultiply(a, 1 / length(a));
+function normaliza(a) {
+  return multiplicacionEscalar(a, 1 / modulo(a));
 }
-function scalarMultiply(a, amount) {
+function multiplicacionEscalar(a, amount) {
   a[0] *= amount;
   a[1] *= amount;
   a[2] *= amount;
@@ -377,7 +389,7 @@ function sumaVectores(v1, v2) {
   v1[2] += v2[2];
   return v1;
 }
-function subtract(v1, v2) {
+function restaVectores(v1, v2) {
   v1[0] -= v2[0];
   v1[1] -= v2[1];
   v1[2] -= v2[2];
@@ -389,14 +401,14 @@ function igualaV1yV2(v1, v2) {
   v1[2] = v2[2];
   return v1;
 }
-function turnOrthogonal(v1) {
+function vuelveOrtogonal(v1) {
   var inverse = 1.0 / Math.sqrt(v1[0] * v1[0] + v1[2] * v1[2]);
   var oldX = v1[0];
   v1[0] = -inverse * v1[2];
   v1[2] = inverse * oldX;
   return v1;
 }
-function crossProduct(v1, v2) {
+function productoVectorial(v1, v2) {
   var oldX = v1[0];
   var oldY = v1[1];
   v1[0] = v2[1] * v1[2] - v2[2] * oldY;
@@ -404,9 +416,9 @@ function crossProduct(v1, v2) {
   v1[2] = v2[0] * oldY - v2[1] * oldX;
   return v1;
 }
-function clampVec(v1, min, max) {
-  v1[0] = clamp(v1[0], min, max);
-  v1[1] = clamp(v1[1], min, max);
-  v1[2] = clamp(v1[2], min, max);
+function restringeVec(v1, min, max) {
+  v1[0] = restriccion(v1[0], min, max);
+  v1[1] = restriccion(v1[1], min, max);
+  v1[2] = restriccion(v1[2], min, max);
   return v1;
 }
